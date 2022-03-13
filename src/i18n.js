5d29b75953a312a46ff6cf47373603b2629d2ac6
techvisionz <info@techvisionz.com>
2022-03-13 07:54:45 +0300

    update

--- src/i18n.js
+++ src/i18n.js
@@ -1,34 +1,23 @@
 import i18n from 'i18next';
 import { initReactI18next, } from 'react-i18next';
 import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-// don't want to use this?
-// have a look at the Quick start guide 
-// for passing in lng and translations on init
+
+const userLanguage = window.navigator.language;
+
 
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
   .use(Backend)
-
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    detection: {
-      // order and from where user language should be detected
-      order: ['sessionStorage'],
-
-      // keys or params to lookup language from
-      lookupSessionStorage: 'i18nextLng',
-      // cache user language on
-    },
     fallbackLng: 'en',
+    lng: localStorage.getItem('language') || userLanguage || 'en',
+    backend: {
+      loadPath: `${process.env.PUBLIC_URL}/locales/{{lng}}/translation.json`
+    },    
+    react: {
+      wait: true,
+      useSuspense: false,
+    },    
     debug: true,
 
     interpolation: {
